bfc119f04ae07b2584f51626e1a4047b978e8f1f
Ian Laidlaw <ilaidlaw@radford.edu>
2024-11-24 22:42:21 -0500

    added bracket-lib as forked submodule

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bevy::prelude::*;
+use bracket_lib::prelude::*;
+
 fn main() {
-    println!("Hello, World!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(BTermBuilder::simple80x50())
+        .add_system(tick)
+        .run();
+}
+
+fn tick(ctx: Res<BracketContext>) {
+    ctx.set_active_console(0);
+    ctx.cls();
+    ctx.print(1, 1, "Hello Bracket-Bevy World ☻");
 }
